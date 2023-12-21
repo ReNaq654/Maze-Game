55274a4a7265afec3a869e1a77f1af4f8b7fa2c2
ReNaq654 <121211738+ReNaq654@users.noreply.github.com>
2023-12-21 16:30:50 -0500

    Added Finish Sound

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,7 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        audioSource.Play();
+        Invoke(nameof(LoadLevel), 1.3f); //Calls LoadLevel() method after 1.3 second delay
+    }
+
+    public void LoadLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
