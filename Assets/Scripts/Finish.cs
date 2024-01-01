42937614f433047a2b87e71a22051b2a95613b72
ReNaq654 <121211738+ReNaq654@users.noreply.github.com>
2024-01-01 13:48:02 -0500

    Fixed Issues with Finish Block
    - Finish Sound would play repeatedly before, now fixed

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -7,10 +7,23 @@ public class Finish : MonoBehaviour
 {
     public AudioSource audioSource;
 
+    private bool levelCompleted = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        audioSource.Play();
-        Invoke(nameof(LoadLevel), 1.3f); //Calls LoadLevel() method after 1.3 second delay
+        if(collision.gameObject.tag == "Player" && !levelCompleted)
+        {
+            audioSource.Play();
+            levelCompleted = true;
+            Invoke(nameof(LoadLevel), 1.3f); //Calls LoadLevel() method after 1.3 second delay
+
+        }
     }
 
     public void LoadLevel()
